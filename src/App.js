e7b28205383fab6298c458332bfa48f7e337f635
piotrostr <piotr.ostrowski@aol.com>
2022-05-06 12:53:12 +0200

    prettier

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import React, { useEffect, useState } from 'react'
-import { useClient, getTheme } from './hooks'
-import TradingViewWidget, { Themes } from 'react-tradingview-widget'
-import styled from 'styled-components'
-
+import React, { useEffect, useState } from 'react';
+import { useClient, getTheme } from './hooks';
+import TradingViewWidget, { Themes } from 'react-tradingview-widget';
+import styled from 'styled-components';
 
 function App() {
-  const [state, setState] = useState({})
-  const [width, setWidth] = useState(window.innerWidth - 50)
-  const client = useClient()
-  const theme = getTheme()
+  const [state, setState] = useState({});
+  const [width, setWidth] = useState(window.innerWidth - 50);
+  const client = useClient();
+  const theme = getTheme();
 
-  useEffect(function() {
-    window.addEventListener('resize', function() {
-      setWidth(window.innerWidth - 50)
-    })
-  }, [])
+  useEffect(function () {
+    window.addEventListener('resize', function () {
+      setWidth(window.innerWidth - 50);
+    });
+  }, []);
 
-  useEffect(async function () {
-    console.log(client)
-    if (state.currentSymbol) {}
-  }, [state.currentSymbol])
+  useEffect(
+    async function () {
+      console.log(client);
+      if (state.currentSymbol) {
+      }
+    },
+    [state.currentSymbol],
+  );
 
   return (
     <ChartScreen>
@@ -31,11 +34,9 @@ function App() {
         height={500}
       />
     </ChartScreen>
-  )
+  );
 }
 
-const ChartScreen = styled.div`
-`
+const ChartScreen = styled.div``;
 
-export default App
-
+export default App;
